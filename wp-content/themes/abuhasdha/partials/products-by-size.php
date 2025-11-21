81daa1700369126378613638fcc843308457c15c
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-21 18:31:52 +0700

    Remove unnecessary flex class from products-by-size section for cleaner layout

--- wp-content/themes/abuhasdha/partials/products-by-size.php
+++ wp-content/themes/abuhasdha/partials/products-by-size.php
@@ -2,7 +2,7 @@
 $products = get_query_var('products');
 ?>
 
-<section class="container mx-auto flex flex-col justify-center items-center py-6 px-6 md:py-10">
+<section class="container mx-auto flex flex-col justify-center py-6 px-6 md:py-10">
     <?php if ($products->have_posts()): ?>
         <?php while ($products->have_posts()): $products->the_post(); ?>
 
